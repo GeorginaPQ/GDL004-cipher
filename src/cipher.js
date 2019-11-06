2988fd69ebdc929c307b1dd74df8f8259f236a6d
PaulinaMontoya <paulinamontoya90@gmail.com>
2019-11-06 09:40:20 -0600

    Funcion Cifrar/Decifrar

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,33 @@
 window.cipher = {
-  // Acá escribe tu código.
-};
+  // Aqui esta escrito mi còdigo
+ encode: function(offset,string) {
+            let y= "";
+            for(let i=0; i<string.length; i++){
+              let x= string.charCodeAt(i);
+              if(x>=65 && x<=90){
+              let ciph= String.fromCharCode ((x-65+offset)%26+65);
+              y= y+ciph;
+              }else{
+              ciph= " ";
+              }    
+             }
+            return y;
+            },
+ decode: function(offset,string){
+           let y= "";
+           for(let i=0; i<string.length; i++){
+           let x= string.charCodeAt(i);
+           if(x>=65 && x<=90){
+           let deciph= String.fromCharCode ((x+65-offset)%26+65);
+           y= y+deciph
+           }else{
+              deciph= " ";
+           }
+             }
+            return y;
+            }
+}
+
+//console.log(cipher.encode(3,"ABC"));
+
+
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
+
+
+
+
+
 let botonIniciar= document.getElementById("iniciar");
 botonIniciar.addEventListener("click", mostrarPantallaCodificar);
 
-//let botonCodificar= document.getElementById("codificar");
-//botonCodificar.addEventListener("click",mostrarTextoCodificado);
-//let botonDecodificar= document.getElementById("decodificar"); 
-//botonDecodificar.addEventListener("click", mostrarTextoDecodificado);
+let botonCodificar= document.getElementById("codificar");
+botonCodificar.addEventListener("click",mostrarTextoCodificado);
+let botonDecodificar= document.getElementById("decodificar"); 
+botonDecodificar.addEventListener("click", mostrarTextoDecodificado);
 let botonApantallaCodificar=document.getElementById("aPantallaCodificar");
  botonApantallaCodificar.addEventListener("click", regresarPantallaCodificar)
 let botonApantallaDecodificar= document.getElementById("aPantallaDecodificar"); 
@@ -26,6 +31,11 @@ function regresarPantallaCodificar(){
     pantallaCodificar.style.display="block";
     let pantallaDecodificar= document.getElementById("pantallaDecodificar");
     pantallaDecodificar.style.display="none"
+    //
+    //
+//document.write
+
+
  }
  function regresarPantallaDecodificar(){
     let pantallaCodificar= document.getElementById("pantallaCodificar");
@@ -33,5 +43,23 @@ function regresarPantallaCodificar(){
     let pantallaDecodificar= document.getElementById("pantallaDecodificar");
     pantallaDecodificar.style.display="block"
  }
+ 
+   function mostrarTextoCodificado(){
+      let string= document.getElementById("textoCodificar").value;
+      let offsetCodificar= document.getElementById("offsetCodificar").value;
+      let offNum = parseInt(offsetCodificar);
+      let resultado = cipher.encode(offNum, string);
+      let sustituir= document.getElementById("textoCodificado");
+      sustituir.innerHTML = "Tu codigo ID es: "+ "<br>" +resultado;
+   }
 
-  
+   function mostrarTextoDecodificado(){
+      let stringD= document.getElementById("textoDecodificar").value;
+      let offsetDecodificar= document.getElementById("offsetDecodificar").value;
+      let offNumD = parseInt(offsetDecodificar);
+      let resultadoD = cipher.decode(offNumD, stringD);
+      let sustituir= document.getElementById("textoDecodificado");
+      sustituir.innerHTML = "Tu ID decodificado es: "+ "<br>" +resultadoD;
+   }
+    
+   
